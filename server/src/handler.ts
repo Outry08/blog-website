3bad31b872ebb103eac391f59b237b1ad2782881
outry08 <outryvball04@gmail.com>
2026-09-17 17:22:38 -0400

    Removed generic server + handler in favour of more modern express + routing flow

--- server/src/handler.ts
+++
@@ -1,6 +0,0 @@
-import { IncomingMessage, ServerResponse } from "http";
-
-export const handler = async (req: IncomingMessage, res: ServerResponse) => {
-	console.log(`Received request: ${req.method} ${req}`);
-	return;
-};
